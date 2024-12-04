67643982d198f06f85881de839ca6d57606ae0f9
Ahmed <141933942+elp4p4@users.noreply.github.com>
2024-12-04 12:06:52 +0100

    Update Task-04.js

--- Task-04.js
+++ Task-04.js
@@ -1,8 +1,10 @@
-async function concurrentRequests() {
-    const simulateApiCall = (id) => new Promise(resolve => {
+function simulateApiCall(id) {
+    return new Promise(resolve => {
         setTimeout(() => resolve(`Data from API ${id}`), 1000 * id);
     });
+}
 
+async function concurrentRequests() {
     const [res_1, res_2] = await Promise.all([
         simulateApiCall(5),
         simulateApiCall(2)
