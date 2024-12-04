c853b2f651f172e6bebfeae775ddae0928179e4f
Ahmed <141933942+elp4p4@users.noreply.github.com>
2024-12-04 12:08:18 +0100

    Update Task-05.js

--- Task-05.js
+++ Task-05.js
@@ -1,8 +1,10 @@
-async function parallelCalls(urls) {
-    const simulateApiCall = (url) => new Promise(resolve => {
+function simulateApiCall(url) {
+    return new Promise(resolve => {
         setTimeout(() => resolve(`Data from ${url}`), 1000);
     });
+}
 
+async function parallelCalls(urls) {
     const responses = await Promise.all(urls.map(url => simulateApiCall(url)));
     console.log("Responses:", responses);
 }
