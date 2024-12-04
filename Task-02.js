fbb834c3fbc8bec4a0f9c8f1f1aba323e28c7090
Ahmed <141933942+elp4p4@users.noreply.github.com>
2024-12-04 12:03:03 +0100

    Update Task-02.js

--- Task-02.js
+++ Task-02.js
@@ -1,10 +1,13 @@
-async function awaitCall() {
-    const simulateApiCall = () => new Promise(resolve => {
+function simulateApiCall() {
+    return new Promise(resolve => {
         setTimeout(() => resolve("API Data"), 2000);
     });
+}
 
+async function awaitCall() {
     const data = await simulateApiCall();
     console.log("Received:", data);
 }
 
 awaitCall();
+
